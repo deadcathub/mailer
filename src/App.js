beb95189c475179344bbe30f968f1c0857c204aa
Vitaly Yugov <yvn@albatrosscom.ru>
2020-01-31 10:17:30 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { LoginCnt } from "./layout/logincnt/LoginCnt";
+import { AuthProvider, AuthConsumer } from "./context/Auth";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      1234
+    </div>
+  );
+}
+
+export default App;
+
+
