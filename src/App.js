f31ca28aa9be0c11a837e9d9851277ae9e181fa4
Vitaly Yugov <yvn@albatrosscom.ru>
2020-01-31 11:16:12 +0700

    Connect bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 // import { LoginCnt } from "./layout/logincnt/LoginCnt";
-import { AuthProvider, AuthConsumer } from "./context/Auth";
-import './App.css';
+// import { AuthProvider, AuthConsumer } from "./context/Auth";
 
 const App = () => {
   return (
-    <div className="App">
-      1234
-    </div>
+    <BrowserRouter>
+      <div className="container pt-4">
+        <h1>Заголовок</h1>
+        <p>
+          Не забывайте, что консультация экстрасенса онлайн может подарить ключ к пониманию
+          самых сложных ситуаций. Попробуйте прямо сейчас! Просто выберите эксперта и нажмите
+          кнопку «Позвонить» рядом с его фотографией.
+        </p>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
 
